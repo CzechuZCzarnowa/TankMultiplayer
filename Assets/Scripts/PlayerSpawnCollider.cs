8508792741456a0ef4146b42b90bc2603cd05b5a
RadekO <rogp2d@gmail.com>
2018-11-07 19:18:37 +0100

    SpawnPlayer fix

---
+++ Assets/Scripts/PlayerSpawnCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerSpawnCollider : MonoBehaviour {
+
+    public bool isOccupied = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            isOccupied = true;
+        }
+
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            isOccupied = false;
+        }
+
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            isOccupied = false;
+        }
+
+    }
+}
